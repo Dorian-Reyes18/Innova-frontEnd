c4475217c974702ac9ba7cde23a7b47498aa7a66
Dorian Reyes <doreyes@gbm.net>
2025-01-10 15:51:45 -0600

    Confgigurando rutas

---
+++ src/pages/login.jsx
@@ -0,0 +1,46 @@
+// src/pages/Login.jsx
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../axios";
+import { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axiosInstance.post("/login", { email, password });
+      localStorage.setItem("authToken", response.data.token);
+      navigate("/home");
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Iniciar sesión</h1>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Correo electrónico"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Iniciar sesión</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;

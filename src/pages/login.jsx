793f54c7271a44643b8da1cbad5d249b9c99f6f4
Dorian Reyes <doreyes@gbm.net>
2025-01-11 07:24:10 -0600

    Creando el login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,18 +1,25 @@
-// src/pages/Login.jsx
 import { useNavigate } from "react-router-dom";
-import axiosInstance from "../axios";
 import { useState } from "react";
+import axiosInstance from "../axios";
+import LogoInnova from "../assets/LogoInnova.svg";
+import KeyIcon from "../assets/Llave.svg";
+import UserIcon from "../assets/User.svg";
+import BolHead from "../assets/bolas-head.svg";
+import BolFoot from "../assets/bolas-foot.svg";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axiosInstance.post("/Login", { email, password });
-      localStorage.setItem("authToken", response.data.token);
+      const response = await axiosInstance.post("/Login", {
+        userName,
+        password,
+      });
+      localStorage.setItem("authToken", response.data.jwt);
       navigate("/Home");
     } catch (error) {
       console.error("Error al iniciar sesión:", error);
@@ -20,25 +27,49 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h1>Iniciar sesión</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Correo electrónico"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Iniciar sesión</button>
-      </form>
+    <div className="login-container">
+      <div className="head">
+        <img src={BolHead} alt="" />
+      </div>
+      <div className="content">
+        <div className="block">
+          <img className="logo" src={LogoInnova} alt="" />
+        </div>
+        <div className="block">
+          <h4>Sistema Gestor</h4>
+          <form onSubmit={handleLogin}>
+            <div className="inp-group">
+              <img src={UserIcon} alt="" />
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Nombre de usuario"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                required
+              />
+            </div>
+            <div className="inp-group">
+              <img src={KeyIcon} alt="" />
+
+              <input
+                className="form-control inputs"
+                type="password"
+                placeholder="Contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button className="btn" type="submit">
+              Iniciar sesión
+            </button>
+          </form>
+        </div>
+      </div>
+      <div className="foot">
+        <img src={BolFoot} alt="" />
+      </div>
     </div>
   );
 };

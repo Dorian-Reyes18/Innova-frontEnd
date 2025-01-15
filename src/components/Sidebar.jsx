35c9fec79ee80cdfd39b6179671f2e7fc8eebaa9
Dorian Reyes <doreyes@gbm.net>
2025-01-15 15:26:26 -0600

    Ajustando el sidebar para renderizar segun rol

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom"; // Corregí "react-route-dom" a "react-router-dom"
+import { useUser } from "../context/UserContext";
 import HomeIcon from "../assets/HomeIcon.svg";
 import UsersIcon from "../assets/UsersIcon.svg";
 import Collapse from "../assets/Collapse.svg";
@@ -10,8 +11,11 @@ import Logout from "./LogOutButton";
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const { user } = useUser();
+  const myUser = user?.user?.data;
   const location = useLocation();
 
+  // Actualiza el estado de la barra lateral según el breakpoint
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
@@ -24,27 +28,36 @@ const Sidebar = () => {
     }
 
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [windowWidth]);
 
   const toggleSidebar = () => {
     setCollapsed(!collapsed);
   };
 
-  // Rutas y sus nombres
+  // Rutas y sus permisos
   const menuItems = [
-    { icon: HomeIcon, name: "Inicio", path: "/Home" },
-    { icon: UsersIcon, name: "Usuarios", path: "/usuarios" },
+    {
+      icon: HomeIcon,
+      name: "Inicio",
+      path: "/Home",
+      roles: [1, 2, 3, 4, 5, 6],
+    },
+    { icon: UsersIcon, name: "Usuarios", path: "/usuarios", roles: [1, 5] },
   ];
+
+  // Filtrar los ítems del menú según el rol del usuario
+  const filteredMenuItems = menuItems.filter((item) =>
+    item.roles.includes(myUser?.role?.id)
+  );
 
   return (
     <div className={`sidebar ${collapsed ? "collapsed" : ""}`}>
       <div
         className={collapsed ? "sidebar-header-collapsed" : "sidebar-header"}
       >
-        {collapsed ? null : (
+        {!collapsed && (
           <img src={InnovaText} alt="InnovaText" className="innovaText" />
         )}
-
         <button onClick={toggleSidebar} className="toggle-btn">
           {collapsed ? (
             <img src={onCollapse} alt="onCollapse" />
@@ -55,9 +68,9 @@ const Sidebar = () => {
       </div>
       {windowWidth < 992 && collapsed ? null : (
         <div className="sidebar-content">
-          <nav className={"sidebar-nav"}>
+          <nav className="sidebar-nav">
             <ul>
-              {menuItems.map((item) => (
+              {filteredMenuItems.map((item) => (
                 <li
                   key={item.path}
                   className={
@@ -66,8 +79,7 @@ const Sidebar = () => {
                 >
                   <Link to={item.path}>
                     <img src={item.icon} alt="" />
-
-                    {collapsed ? null : item.name}
+                    {!collapsed && item.name}
                   </Link>
                 </li>
               ))}
